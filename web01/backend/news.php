cb1c5461788b48701971f6d5763655a131c4acb0
maggiechen <chenmaggiejob@gmail.com>
2024-06-19 09:42:28 +0800

    新增分頁功能

--- web01/backend/news.php
+++ web01/backend/news.php
@@ -10,8 +10,12 @@
 
                 </tr>
                 <?php
-
-                $rows = ${ucfirst($do)}->all();
+                $total = ${ucfirst($do)}->count();
+                $div = 4;
+                $pages = ceil($total / $div);
+                $now = $_GET['p'] ?? 1;
+                $start = ($now - 1) * $div;
+                $rows = ${ucfirst($do)}->all(" limit $start,$div");
                 foreach ($rows as $row) {
 
                 ?>
@@ -32,20 +36,46 @@
                 ?>
             </tbody>
         </table>
-        <table style=" margin-top:40px; width:70%;">
-            <tbody>
-                <tr>
-                    <td width="200px">
-                        <input type="button" onclick="op('#cover','#cvr','./modals/<?= $do; ?>.php')" value="新增最新消息">
-                    </td>
-                    <td class="cent">
-                        <input type="hidden" name="table" value="<?= $do; ?>">
-                        <input type="submit" value="修改確定">
-                        <input type="reset" value="重置">
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        <div class='cent'>
+            <?php
+            if ($now - 1 >= 1) {
+                $prev = $now - 1;
+                echo "<a href='?do=$do&p=$prev'> ";
+                //echo "&lt;";
+                echo "<";
+                echo "</a>";
+            }
+
+            for ($i = 1; $i <= $pages; $i++) {
+                $size = ($i == $now) ? "24px" : "18px";
+                echo "<a href='?do=$do&p=$i' style='font-size:$size'> ";
+                echo $i;
+                echo " </a>";
+            }
+
+            if ($now + 1 <= $pages) {
+                $next = $now + 1;
+                echo "<a href='?do=$do&p=$next'> ";
+                //echo "&gt;";
+                echo ">";
+                echo "</a>";
+            }
+
+            ?>
+            <table style=" margin-top:40px; width:70%;">
+                <tbody>
+                    <tr>
+                        <td width="200px">
+                            <input type="button" onclick="op('#cover','#cvr','./modals/<?= $do; ?>.php')" value="新增最新消息">
+                        </td>
+                        <td class="cent">
+                            <input type="hidden" name="table" value="<?= $do; ?>">
+                            <input type="submit" value="修改確定">
+                            <input type="reset" value="重置">
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
 
     </form>
 </div>
