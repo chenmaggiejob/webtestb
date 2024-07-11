ac613b879f6fc251fbb3031e295ce82ec9bda22c
maggiechen <chenmaggiejob@gmail.com>
2024-07-11 09:37:38 +0800

    增加讚圖示

--- web02/frontend/pop.php
+++ web02/frontend/pop.php
@@ -1,7 +1,7 @@
 <div>
     目前位置：首頁 > 人氣文章區
 </div>
-<table class="tab">
+<table class="tab pop">
     <tr>
         <td width="30%">標題</td>
         <td width="50%">內容</td>
@@ -17,11 +17,25 @@
     foreach ($rows as $idx => $row) {
     ?>
         <tr>
-            <td><?= $row['title']; ?></td>
-            <td>
-                <?= mb_substr($row['article'], 0, 30); ?>
+            <td class='pop-header'><?= $row['title']; ?></td>
+            <td class='pop-header'>
+                <div class="short">
+                    <?= mb_substr($row['article'], 0, 30); ?>
+                </div>
+                <div class="alert">
+                    <div style='font-size:20px;color:skyblue'>
+                        <?php
+                        $type = ['', '健康新知', '菸害防治', '癌症防治', '慢性病防治'];
+                        echo $type[$row['type']];
+                        ?>
+                    </div>
+                    <?= nl2br($row['article']); ?>
+                </div>
             </td>
-            <td><?= $row['good']; ?></td>
+            <td>
+                <?= $row['good']; ?>個人說
+                <img src="./icon/02B03.jpg" style="width: 20px;">
+            </td>
         </tr>
     <?php
     }
@@ -46,3 +60,15 @@
     ?>
 
 </div>
+
+<script>
+    $(".pop-header").hover(
+        function() {
+            $(this).parent().find('.alert').show()
+        },
+        function() {
+            $(this).parent().find('.alert').hide()
+        },
+
+    )
+</script>
