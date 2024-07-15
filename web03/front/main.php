dd597c8bd0226ef32c7d4ee0b764a2fe8ac8d4db
maggiechen <chenmaggiejob@gmail.com>
2024-07-15 13:54:59 +0800

    增加預告片ANI效果輪播功能

--- web03/front/main.php
+++ web03/front/main.php
@@ -3,16 +3,82 @@
     <div class="rb tab" style="width:95%;">
         <div id="posters">
             <div class="lists">
-                <div class='poster'>
-                    <img src="./images/03A07.jpg">
-                </div>
-                <div class='name'>預告片1</div>
+                <?php
+                $posters = $Poster->all(['sh' => 1], " Order By rank");
+                foreach ($posters as $poster) {
+                ?>
+                    <div class='poster' data-ani='<?= $poster['ani']?>'>
+                        <img src="./images/<?= $poster['img']; ?>">
+                        <div class='name'><?= $poster['name']; ?></div>
+                    </div>
+                <?php
+
+                }
+                ?>
             </div>
             <div class="controls">
+                <div class="control">
+                    <div class="btn-left"></div>
+                </div>
+                <div class="btns">
+                    <div class="btn">A</div>
+                    <div class="btn">B</div>
+                    <div class="btn">C</div>
+                    <div class="btn">D</div>
+                </div>
+                <div class="control">
+                    <div class="btn-right"></div>
+                </div>
             </div>
         </div>
     </div>
 </div>
+
+<script>
+    $(".poster").eq(0).show()
+    let now = 0;
+    let next = 1;
+    let slide = setInterval(() => {
+        ani()
+    }, 3000)
+
+    function ani() {
+
+        let ani = parseInt($(".poster").eq(now).data('ani'))
+
+        if (now < $(".poster").length - 1) {
+            next = now + 1;
+        } else {
+            next = 0
+        }
+        switch (ani) {
+            case 1:
+                //淡入淡出
+                $(".poster").eq(now).fadeOut(1000, () => {
+                    $(".poster").eq(next).fadeIn(1000)
+                })
+                break;
+            case 2:
+                //縮放
+                $(".poster").eq(now).hide(1000, () => {
+                    $(".poster").eq(next).show(1000)
+                })
+                break;
+            case 3:
+                //滑入滑出
+                $(".poster").eq(now).slideUp(1000, () => {
+                    $(".poster").eq(next).slideDown(1000)
+                })
+                break;
+
+        }
+        now = next
+
+    }
+</script>
+
+
+
 <div class="half">
     <h1>院線片清單</h1>
     <div class="rb tab" style="width:95%;">
