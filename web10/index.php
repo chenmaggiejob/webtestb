866b40924975bd215677d7e599571c1dd2958beb
maggiechen <chenmaggiejob@gmail.com>
2024-08-23 09:15:39 +0800

    index插入title圖片與頁面調整

--- web10/index.php
+++ web10/index.php
@@ -21,7 +21,7 @@
 		<div id="title">
 			00 月 00 號 Tuesday | 今日瀏覽: 1 | 累積瀏覽: 36 </div>
 		<div id="title2">
-
+			<img src="./icon/02B01.jpg" alt="">
 		</div>
 		<div id="mm">
 			<div class="hal" id="lef">
