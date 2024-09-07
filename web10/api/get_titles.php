fbda489670dc8afb37bdbd03b8c65aaa2c2b63de
maggie <maggie@maggies-MacBook-Air.local>
2024-09-07 20:08:29 +0800

    變更標籤

--- web10/api/get_titles.php
+++ web10/api/get_titles.php
@@ -4,9 +4,9 @@ include_once "base.php";
 $news = $News->all(['type' => $_POST['type']]);
 
 foreach ($news as $n) {
-    echo "<pre>";
+    echo "<p>";
     echo "<a href='javascript:getNews({$n['id']})'>";
     echo $n['title'];
     echo "</a>";
-    echo "</pre>";
+    echo "</p>";
 }
