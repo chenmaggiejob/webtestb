5970b8a5928973cd3c2234163ac30ce1ef2f2ff0
maggie <maggie@maggies-MacBook-Air.local>
2024-09-07 23:08:25 +0800

    web10-2 完成frontend/po 功能

--- web10-2/frontend/po.php
+++ web10-2/frontend/po.php
@@ -1,6 +1,19 @@
 <div>
-    目前位置：首頁 > 分類網誌 > <span id="navType">健康新知</span>
+    目前位置：首頁 > 分類文章 > <span id="navType">健康新知</span>
 </div>
+<!-- <div style="align-items:start;display:flex;">
+    <fieldset style="padding: 10px;width:150px;">
+        <legend>分類文章</legend>
+        <a class="type" data-type="1" style="margin: 5px; display:block;">健康新知</a>
+        <a class="type" data-type="2" style="margin: 5px; display:block;">菸害防治</a>
+        <a class="type" data-type="3" style="margin: 5px; display:block;">癌症防治</a>
+        <a class="type" data-type="4" style="margin: 5px; display:block;">慢性病防治</a>
+    </fieldset>
+    <fieldset style="padding: 10px;width:550px;">
+        <legend id="newTitle">文章列表</legend>
+        <div id="content"></div>
+    </fieldset>
+</div> -->
 
 <div style="align-items: start; display: flex;">
     <fieldset style="padding: 10px;width: 150px;">
@@ -16,9 +29,9 @@
     </fieldset>
 </div>
 
-<script>
-    getTitles(1)
 
+
+<script>
     $(".type").on("click", function() {
         $("#navType").text($(this).text())
         getTitles($(this).data('type'))
@@ -28,7 +41,6 @@
         $("#content").load("./api/get_titles.php", {
             type
         })
-
     }
 
     function getNews(id) {
