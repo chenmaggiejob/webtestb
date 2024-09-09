e8ce1edded1679ea1f4a3c560073cd0b21de4cec
maggiechen <chenmaggiejob@gmail.com>
2024-09-09 10:20:29 +0800

    新增練習專案

---
+++ web0909/api/base.php
@@ -0,0 +1,133 @@
+<?php
+session_start();
+
+class DB
+{
+    protected $table;
+    protected $dsn = "mysql:host=localhost;charset=utf8;dbname=db10";
+    protected $pdo;
+
+    function __construct($table)
+    {
+        $this->table = $table;
+        $this->pdo = new PDO($this->dsn, 'root', '');
+    }
+
+    function all(...$arg)
+    {
+        $sql = "select * from `$this->table`";
+        if (isset($arg[0])) {
+            if (is_array($arg[0])) {
+                $tmp = $this->a2s($arg[0]);
+                $sql .= " where " . join(" && ", $tmp);
+            } else {
+                $sql .= $arg[0];
+            }
+        }
+        if (isset($arg[1])) {
+            $sql .= $arg[1];
+        }
+        echo $sql;
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    function find($arg)
+    {
+        $sql = "select * from `$this->table`";
+        if (is_array($arg)) {
+            $tmp = $this->a2s($arg);
+            $sql .= " where " . join(" && ", $tmp);
+        } else {
+            $sql .= " where `id` = '$arg'";
+        }
+        echo $sql;
+        return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+    }
+    function del($arg)
+    {
+        $sql = "delete from `$this->table`";
+        if (is_array($arg)) {
+            $tmp = $this->a2s($arg);
+            $sql .= " where " . join(" && ", $tmp);
+        } else {
+            $sql .= " where `id` = '$arg'";
+        }
+        // echo $sql;
+        return $this->pdo->exec($sql);
+    }
+
+    function save($arg)
+    {
+        if (isset($arg['id'])) {
+            $tmp = $this->a2s($arg);
+            $sql = "update `$this->table` set " . join(",", $tmp) . " where `id` = {$arg['id']}";
+        } else {
+            $keys = array_keys($arg);
+            $sql = "insert into `$this->table` (`" . join("`,`", $keys) . "`)
+            values ('" . join("','", $arg) . "')";
+        }
+        echo $sql;
+        return $this->pdo->exec($sql);
+    }
+
+    function count(...$arg)
+    {
+        $sql = "select count(*) from `$this->table`";
+        if (isset($arg[0])) {
+            if (is_array($arg[0])) {
+                $tmp = $this->a2s($arg[0]);
+                $sql .= " where " . join(" && ", $tmp);
+            } else {
+                $sql .= $arg[0];
+            }
+        }
+        if (isset($arg[1])) {
+            $sql .= $arg[1];
+        }
+        echo $sql;
+        return $this->pdo->query($sql)->fetchColumn();
+    }
+
+    function a2s($array)
+    {
+        $tmp = [];
+        foreach ($array as $key => $value) {
+            $tmp[] = "`$key`='$value'";
+        }
+        return $tmp;
+    }
+}
+
+$User = new DB('users');
+$users = $User->del(['id' => '28']);
+// UPDATE `users` SET `acc` = '090901' WHERE `users`.`id` = 29;
+function q($sql)
+{
+    $dsn = "mysql:host=localhost;charset=utf8;dbname=db10";
+    $pdo = new PDO($dsn, 'root', '');
+    return $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function to($url)
+{
+    header("location:" . $url);
+}
+
+
+function dd($array)
+{
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+}
+
+if (isset($_SESSION['total'])) {
+    if ($Total->count(['date' => date("Y-m-d")]) > 0) {
+        $total = $Total->find(['date' => date("Y-m-d")]);
+        $total['total']++;
+        $Total->save($total);
+    } else {
+        $Total->save(['date' => date("Y-m-d"), 'total' => 1]);
+    }
+    $_SESSION['total'] = $Total->find(['date' => date("Y-m-d")])['total'];
+}
